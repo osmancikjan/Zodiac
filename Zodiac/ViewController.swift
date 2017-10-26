393f9e9b709579b558df6d034253a03ed87d6fec
Jan Osmančík <j.osmancik@gmail.com>
2017-10-26 22:37:17 +0200

    Initial Commit

---
+++ Zodiac/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Zodiac
+//
+//  Created by Jan Osmancik on 26.10.17.
+//  Copyright © 2017 Jan Osmancik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
